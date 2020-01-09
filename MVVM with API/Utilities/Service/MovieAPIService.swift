d2cef06ace997d972f382caa68ca33881f5e2f1d
Asep Abdaz <asepabdaz@gmail.com>
2020-01-09 12:19:14 +0700

    Added MovieAPIService Replaced Networking

---
+++ MVVM with API/Utilities/Service/MovieAPIService.swift
@@ -0,0 +1,14 @@
+//
+//  MoveAPIService.swift
+//  MVVM with API
+//
+//  Created by Asep Abdaz on 09/01/20.
+//  Copyright © 2020 Asep Abdaz. All rights reserved.
+//
+
+import UIKit
+
+class MovieAPIService: NSObject, Requestable {
+    
+    
+}
