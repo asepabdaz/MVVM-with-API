5258eb64d5f715e104222a3034625514c0c3f962
Asep Abdaz <asepabdaz@gmail.com>
2020-01-09 12:01:24 +0700

    Create Networking.swift create function request and gat request to managed data from API

---
+++ MVVM with API/Utilities/Networking.swift
@@ -0,0 +1,99 @@
+//
+//  Networking.swift
+//  MVVM with API
+//
+//  Created by Asep Abdaz on 09/01/20.
+//  Copyright © 2020 Asep Abdaz. All rights reserved.
+//
+
+import UIKit
+
+/**
+    Enum Result  digunakan untuk menyimpan data status. Penggunaan Enum di API  dapat kamu baca di dokumentasi.
+    **SwiftBySundell**
+    https://www.swiftbysundell.com/posts/the-power-of-result-types-in-swift
+ */
+enum Result<Value: Decodable> {
+    case success(Value)
+    case failure(Bool)
+}
+
+typealias Handler = (Result<Data?>) -> Void
+
+enum NetworkError: Error {
+    case nullData
+}
+
+public enum Method {
+    case get
+    case post
+    case other(method: String)
+}
+
+enum NetworkingError: String, LocalizedError {
+    case jsonError = "JSON error"
+    case other
+    var localizedDescription: String { return NSLocalizedString(self.rawValue, comment: "") }
+}
+
+extension Method {
+    public init(_ rawValue: String) {
+        let method = rawValue.uppercased()
+        switch method {
+        case "GET":
+            self = .get
+        case "POST":
+            self = .get
+        default:
+            self = .other(method: method)
+        }
+    }
+}
+
+extension Method: CustomStringConvertible{
+    public var description: String {
+        switch self {
+        case .get:
+            return "DELETE"
+        case .post:
+            return "POST"
+        case .other(let method):
+            return method.uppercased()
+        }
+    }
+}
+
+protocol Requestable {}
+
+extension Requestable {
+    internal func getRequest(url: String, callBack: @escaping (_ json: NSDictionary?) -> ()) {
+        
+            request(method: .get, url: url, params: nil) { (dict) in
+                
+            }
+        
+    }
+    
+    internal func request(method: Method, url: String, params: [NSString: Any]? = nil, callback: @escaping Handler){
+        
+        guard let url = URL(string: url) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            DispatchQueue.main.async {
+                if let error = error {
+                    print(error.localizedDescription)
+                }else if let httpResponse = response as? HTTPURLResponse {
+                    if httpResponse.statusCode == 200 {
+                        // TODO: decode data JSON to model
+                        
+                        
+                    }else {
+                        callback(.failure(true))
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+}
