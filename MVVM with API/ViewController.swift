d516802855f3bbb701191a498ac0f8175580832b
Asep Abdaz <asepabdaz@gmail.com>
2020-01-09 09:20:34 +0700

    Initial Commit

---
+++ MVVM with API/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM with API
+//
+//  Created by Asep Abdaz on 09/01/20.
+//  Copyright © 2020 Asep Abdaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVVM with APITests/MVVM_with_APITests.swift
@@ -0,0 +1,34 @@
+//
+//  MVVM_with_APITests.swift
+//  MVVM with APITests
+//
+//  Created by Asep Abdaz on 09/01/20.
+//  Copyright © 2020 Asep Abdaz. All rights reserved.
+//
+
+import XCTest
+@testable import MVVM_with_API
+
+class MVVM_with_APITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
