03ef4211a7a34040c01902953d34e10c3f5e5efb
Asep Abdaz <asepabdaz@gmail.com>
2020-01-29 14:16:07 +0700

    test

---
+++ MVVM with API/View/TestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TestViewController.swift
+//  MVVM with API
+//
+//  Created by Asep Abdaz on 29/01/20.
+//  Copyright © 2020 Asep Abdaz. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
